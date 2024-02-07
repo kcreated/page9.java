9db6025189a448fbf0a360b256839e203be80b67
kcreated <kyciabt@gmail.com>
2024-02-07 14:44:44 -0500

    u
    gh

---
+++ partofpage9.java
@@ -0,0 +1,15 @@
+/***********
+ * Programmer: Kate Chae
+ * Date: Feburary 7th
+ * Program Name:partofpage9
+ *********/
+
+import java.io.*;
+public class partofpage9
+{
+public static void main (String[ ] args)
+{
+System.out.println("Hi"); //message to user
+System.out.println("LabOneA"); //assignment
+}
+}
